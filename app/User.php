144c7a81f6a01afd6fe4fb55fa5a81fa10506155
Thanh Trung <trung9x.dev@gmail.com>
2019-08-25 14:49:04 +0700

    edit $fillable

--- app/User.php
+++ app/User.php
@@ -16,32 +16,31 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
         'username',
         'password',
-        'full_name',
+        'name',
         'email',
         'phone',
         'address'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+//    /**
+//     * The attributes that should be hidden for arrays.
+//     *
+//     * @var array
+//     */
+//    protected $hidden = [
+//        'password', 'remember_token',
+//    ];
+//
+//    /**
+//     * The attributes that should be cast to native types.
+//     *
+//     * @var array
+//     */
+//    protected $casts = [
+//        'email_verified_at' => 'datetime',
+//    ];
 
     public function user_roles() {
         return $this->hasMany('App\UserRole');
